b84e1c1c726719a33ec73aca1cecf5c477c83137
Ryan <ryanwanjiedistortion@gmail.com>
2023-06-14 12:35:06 +0300

    tours

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,10 +5,17 @@ import '../styles/login.css';
 
 import loginImg from '../assets/images/login.png';
 import userIcon from '../assets/images/user.png';
+import axios from 'axios'
+import {useNavigate} from 'react-router-dom'
+import SweetAlert from 'react-bootstrap-sweetalert';
+
+
 
 
 const Login = () => {
 
+
+  const navigate = useNavigate()
 
 //   const [credentials, setCredentials] = useState({
 //     email: undefined,
@@ -21,13 +28,68 @@ const Login = () => {
 
 
 const [email, setemail] = useState('')
+const [password, setpassword] = useState('')
+const [error, seterror] = useState('')
+const [succ, setsucc] = useState(false)
 
 
+const handleemail = (e)=>{
+
+  setemail(e.target.value)
+}
+
+const handlepassword = (e)=>{
+
+  setpassword(e.target.value)
+}
 
 
 
 const handleClick = e =>{
   e.preventDefault();
+
+  if(!email || !password){
+
+    seterror('Please provide the full details')
+
+    return
+  }
+
+
+  try{
+
+    const logindata = {
+
+      email: email,
+      password: password
+    }
+
+    axios.post('http://localhost:3004/api/auth/login', logindata)
+
+    .then(res=>{
+
+      console.log(logindata)
+      setsucc(true)
+
+    })
+
+    setTimeout(()=>{
+
+      navigate('/home')
+
+    },2000)
+
+
+
+  }
+
+  catch(err){
+
+    err.response.data.msg && seterror(err.response.data.msg)
+
+  }
+
+
   
 };
 
@@ -48,15 +110,25 @@ const handleClick = e =>{
               <h2>Login</h2>
 
               <Form onSubmit={handleClick}>
+
+
                 <FormGroup>
-                  <input type="email" placeholder='E-mail' required id='email' onChange={handleChange}/>
+                  <input type="email" placeholder='E-mail' required id='email' value ={email} onChange={handleemail}/>
                 </FormGroup>
                 <FormGroup>
-                  <input type="password" placeholder='PASSWORD' required id='password' onChange={handleChange}/>
+                  <input type="password" placeholder='PASSWORD' required id='password' value={password} onChange={handlepassword}/>
                 </FormGroup>
                 <Button className='btn secondary__btn auth__btn' type='submit'>
                   Login
                 </Button>
+
+                {succ && (
+                <SweetAlert success title="Login Successful" onConfirm={() => setsucc(false)}>
+                  Your registration was successful!
+                </SweetAlert>
+              )}
+
+
               </Form>
               <p>Don't have an account <Link to='/registre'>Create</Link></p>
             </div>
